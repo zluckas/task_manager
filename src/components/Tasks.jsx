56a68d1999d2fcebb9b713b1f833e2c4e89d24d2
Lucas Nóbrega <lucasfernanndo07@gmail.com>
2025-12-10 17:55:18 -0300

    Adição de Query Params

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, SquarePlus, TabletSmartphone, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({tasks, onDeleteTaskClick, onTaskClick}) {
+  const navigator = useNavigate();
+
+  function onSeeDetailsClick(task){
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigator(`/task?${query.toString()}`);
+  }
+  
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -14,7 +24,8 @@ function Tasks({tasks, onDeleteTaskClick, onTaskClick}) {
             {task.title}
           </button>
 
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button onClick={() => onSeeDetailsClick(task)} 
+          className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
           </button>
 
